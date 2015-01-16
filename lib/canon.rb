7fbb65dfd209f4ab72c542ef35e8455ac44425b4
JScott <jvscott@gmail.com>
2015-01-16 18:01:40 -0500

    Printing more clear as to what I care about

--- lib/canon.rb
+++ lib/canon.rb
@@ -3,14 +3,17 @@ require 'moneta'
 established = Dir.exists? '.canon'
 
 archives = Moneta.new :File, dir: '.canon'
-p "Canonical Execution Established as #{archives.fetch 'canon', []}"
+puts "Canonical Execution Established as", "#{archives.fetch 'canon', []}"
 
 TracePoint.trace do |t|
   canon = archives.fetch 'canon', []
   case t.event
   when :return, :b_return
-    parameters = t.binding.eval "p method(__method__).parameters.map { |p| eval p.last.to_s }"
+    parameters = t.binding.eval "method(__method__).parameters.map { |p| eval p.last.to_s }"
     canon << [t.method_id, parameters, t.return_value]
   end
   archives.store 'canon', canon unless established
 end
+
+puts
+puts "Current Execution is", "#{archives.fetch 'canon', nil}"
