433242a1d48dbbb3fdfcf4e48a25d54e13bb8854
JScott <jvscott@gmail.com>
2015-01-23 15:22:17 -0800

    Moving output dependencies to its own function

--- lib/canon.rb
+++ lib/canon.rb
@@ -3,46 +3,47 @@ require 'moneta'
 
 established_canon = false
 storage = Moneta.new :File, dir: '.canon'
-storage.delete 'identity'
-storage.delete 'method_chain'
 
-puts "Established Canon is\n#{storage.fetch 'canon', []}\n---\n"
+puts '---', "Identity: #{storage.fetch 'identity', []}"
+puts '---', "Output deps: #{storage.fetch 'method_dependencies', []}"
+
+storage.delete 'identity'
+storage.delete 'method_dependencies'
+
+puts '---', "Established Canon is\n#{storage.fetch 'canon', []}"
 storage.delete 'canon' unless established_canon
 
 
+def output_dependencies(previous_method_calls, method_call)
+  puts "Checking deps for #{method_call[:name]}"
+  dependencies = []
+  previous_method_calls.each do |previous_method_call|
+    output = previous_method_call[:output]
+    method_call[:input].each do |input|
+      #puts "i: #{input.inspect} (#{input.object_id})"
+      #puts "o: #{output.inspect} (#{output.object_id})"
+      link = [previous_method_call[:name], method_call[:name]]
+      dependencies.push link if output == input
+    end
+  end
+  dependencies
+end
+
 TracePoint.trace do |t|
   identity = storage.fetch 'identity', []
-  method_chain = storage.fetch 'method_chain', []
+  method_dependencies = storage.fetch 'method_dependencies', []
   case t.event
   when :return, :b_return
-    get_parameters = "method(__method__).parameters.map { |p| eval p.last.to_s }"
-    parameters = t.binding.eval get_parameters
+    parameters = t.binding.eval "method(__method__).parameters.map { |p| eval p.last.to_s }"
     method_call = { name: t.method_id, input: parameters, output: t.return_value }
-    
-    identity.each do |method|
-      puts '>>>'
-      method[:input].each do |input|
-        output = method[:output]
-        puts "Running #{method[:name]}"
-        puts "A: #{input.inspect} (#{input.object_id})"
-        puts "B: #{output.inspect} (#{output.object_id})"
-        other_methods = identity.reject { |m| m == method }
-        puts other_methods.inspect
-      end
-      puts '<<<'
-    end
-    puts "=== MASTER POKE ==="
-    
+    output_dependencies = output_dependencies(identity, method_call)
+
+    method_dependencies.concat output_dependencies unless output_dependencies.empty?
     identity.push method_call
-    #puts identity.inspect
   end
   storage.store 'identity', identity
-  storage.store 'method_chain', method_chain
   storage.store 'canon', identity unless established_canon
-
-  #p identity if identity.any?
-  #explore storage for where this input came from
-  # add this method to 
+  storage.store 'method_dependencies', method_dependencies
 end
 
 # don't put anything here unless you want it traced
