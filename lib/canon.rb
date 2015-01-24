d3722a2f381d92abfea9dae4e19127026ed817da
JScott <jvscott@gmail.com>
2015-01-23 16:07:49 -0800

    Less terrible nesting

--- lib/canon.rb
+++ lib/canon.rb
@@ -12,37 +12,38 @@ storage.delete 'identity'
 storage.delete 'method_dependencies'
 storage.delete 'canon' unless established_canon
 
-def trace_output(previous_method_calls, method_call)
-  dependencies = []
-  previous_method_calls.each do |previous_method_call|
-    output = previous_method_call[:output]
-    method_call[:input].each do |input|
-      link = {
-        output: previous_method_call[:output],
-        from: previous_method_call[:name],
-        to: method_call[:name]
-      }
-      dependencies.push link if output == input
-    end
+def match(input:, to_output:)
+
+end
+
+def trace_output(from_method_calls: [], to:)
+  matches = from_method_calls.select do |previous_method_call|
+    to[:input].include? previous_method_call[:output]
   end
-  dependencies
+  matches.map do |match|
+    {
+      output: match[:output],
+      from: match[:name],
+      to: to[:name]
+    }
+  end
 end
 
 TracePoint.trace do |t|
-  previous_method_calls = storage.fetch 'identity', []
+  identity = storage.fetch 'identity', []
   method_dependencies = storage.fetch 'method_dependencies', []
   case t.event
   when :return  #, :b_return
     parameters = t.binding.eval "method(__method__).parameters.map { |p| eval p.last.to_s }"
     method_call = { name: t.method_id, input: parameters, output: t.return_value }
-    output_dependencies = trace_output previous_method_calls, method_call
+    output_dependencies = trace_output from_method_calls: identity, to: method_call
 
     method_dependencies.concat output_dependencies unless output_dependencies.empty?
-    previous_method_calls.push method_call
+    identity.push method_call
   end
-  storage.store 'identity', previous_method_calls
+  storage.store 'identity', identity
   storage.store 'method_dependencies', method_dependencies
-  storage.store 'canon', previous_method_calls unless established_canon
+  storage.store 'canon', identity unless established_canon
 end
 
 # don't put anything here unless you want it traced
