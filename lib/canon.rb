0d507172bf69b96f37098c3735d57c3b9cc01928
JScott <jvscott@gmail.com>
2015-01-24 20:49:34 -0800

    Code clean up

--- lib/canon.rb
+++ lib/canon.rb
@@ -1,6 +1,5 @@
 require 'moneta'
 
-
 established_canon = false
 storage = Moneta.new :File, dir: '.canon'
 
@@ -8,7 +7,7 @@ puts '---', "Identity\n#{storage.fetch 'identity', []}"
 puts '---', "Method dependencies\n#{storage.fetch 'method_dependencies', []}"
 puts '---', "Established Canon is\n#{storage.fetch 'canon', []}"
 
-def trace_output(from_method_calls: [], to:)
+def map_output(from_method_calls:, to:)
   matches = from_method_calls.select do |previous_method_call|
     to[:input].index { |input| input.equal? previous_method_call[:output] }
   end
@@ -21,23 +20,28 @@ def trace_output(from_method_calls: [], to:)
   end
 end
 
-TracePoint.trace do |t|
-  @identity ||= []
-  method_dependencies = storage.fetch 'method_dependencies', []
-  case t.event
-  when :return
-    parameters = t.binding.eval "method(__method__).parameters.map { |p| eval p.last.to_s }"
-    method_call = { name: t.method_id, input: parameters, output: t.return_value }
-    output_dependencies = trace_output from_method_calls: @identity, to: method_call
+def new_method_call(from:)
+  parameters = from.binding.eval "method(__method__).parameters.map { |p| eval p.last.to_s }"
+  {
+    name: from.method_id,
+    input: parameters,
+    output: from.return_value
+  }
+end
 
-    method_dependencies.concat output_dependencies unless output_dependencies.empty?
-    @identity.push method_call
+TracePoint.trace do |trace|
+  @identity ||= []
+  @dependencies ||= []
+  case trace.event
+  when :return
+    @identity.push new_method_call from: trace
+    @dependencies.concat map_output from_method_calls: @identity[0...-1], to: @identity.last
   when :b_return, :c_return
   else
   end
-  storage.store 'identity', @identity
-  storage.store 'method_dependencies', method_dependencies.uniq
-  storage.store 'canon', @identity unless established_canon
+  storage.store 'identity', @identity.uniq
+  storage.store 'dependencies', @dependencies.uniq
+  storage.store 'canon', @identity.uniq unless established_canon
 end
 
 # don't put anything here unless you want it traced
