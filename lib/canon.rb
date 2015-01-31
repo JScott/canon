484101246fb8db5503763e8fdcc941679a429e96
JScott <jvscott@gmail.com>
2015-01-31 13:36:31 -0800

    Dependencies work as expected

--- lib/canon.rb
+++ lib/canon.rb
@@ -6,24 +6,6 @@ puts '---', "Method calls\n#{storage.fetch 'calls', []}"
 puts '---', "Method returns\n#{storage.fetch 'returns', []}"
 puts '---', "Method dependencies\n#{storage.fetch 'dependencies', []}"
 
-#def match_output_to_input(previous_method_calls, current)
-#  previous_method_calls.select do |previous|
-#    current[:input].detect { |given_input| given_input.equal? previous[:output] }
-#  end
-#end
-
-#def output_dependencies(from_method_calls:, to:)
-#  #puts '---', from_method_calls.inspect, to.inspect
-#  matches = match_output_to_input(from_method_calls, to)
-#  matches.map do |match|
-#    {
-#      output: match[:output],
-#      from: match[:name],
-#      to: to[:name]
-#    }
-#  end
-#end
-
 def new_method_call(from:)
   parameters = from.binding.eval "method(__method__).parameters.map { |p| eval p.last.to_s }"
   {
@@ -39,8 +21,20 @@ def new_method_return(from:)
   }
 end
 
-def trace_method_calls_to(method_return)
-  []
+def returns_that_pass_to(current)
+  @returns.select do |previous|
+    current[:input].detect { |given_input| given_input.equal? previous[:output] }
+  end
+end
+
+def dependencies_for(method_call)
+  returns_that_pass_to(method_call).map do |method_return|
+    {
+      output: method_return[:output],
+      from: method_return[:name],
+      to: method_call[:name]
+    }
+  end
 end
 
 TracePoint.trace do |trace|
@@ -49,14 +43,11 @@ TracePoint.trace do |trace|
   @dependencies ||= []
   case trace.event
   when :call
-    parameters = trace.binding.eval "method(__method__).parameters.map { |p| eval p.last.to_s }"
-    @calls.push new_method_call(from: trace)
+    method_call = new_method_call from: trace
+    @dependencies.concat dependencies_for(method_call)
+    @calls.push method_call
   when :return
-    method_return = new_method_return from: trace
-    @dependencies.concat trace_method_calls_to(method_return)
-    #@dependencies.concat output_dependencies from_method_calls: @identity, to: method_call
-    #@dependencies.concat internal_dependencies from_method_calls: @identity, to: method_call
-    @returns.push method_return
+    @returns.push new_method_return(from: trace)
   when :b_return, :c_return
   else
   end
