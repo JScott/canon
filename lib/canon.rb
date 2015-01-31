c9b204141df567d9b6df5d8fce1c66c1926a3ed4
JScott <jvscott@gmail.com>
2015-01-31 13:10:15 -0800

    Separated returns and calls
    By the time we call a return trace, the input may have changed. Because of this, we have to separate the two. Luckily, we don't actually care which call leads to which return! At least not yet...

--- lib/canon.rb
+++ lib/canon.rb
@@ -1,65 +1,68 @@
 require 'moneta'
 
-established_canon = false
 storage = Moneta.new :File, dir: '.canon'
 
-puts '---', "Identity\n#{storage.fetch 'identity', []}"
+puts '---', "Method calls\n#{storage.fetch 'calls', []}"
+puts '---', "Method returns\n#{storage.fetch 'returns', []}"
 puts '---', "Method dependencies\n#{storage.fetch 'dependencies', []}"
-puts '---', "Established Canon is\n#{storage.fetch 'canon', []}"
 
-def match_output_to_input(previous_method_calls, current)
-  previous_method_calls.select do |previous|
-    current[:input].detect { |given_input| given_input.equal? previous[:output] }
-  end
-end
+#def match_output_to_input(previous_method_calls, current)
+#  previous_method_calls.select do |previous|
+#    current[:input].detect { |given_input| given_input.equal? previous[:output] }
+#  end
+#end
 
-def output_dependencies(from_method_calls:, to:)
-  #puts '---', from_method_calls.inspect, to.inspect
-  matches = match_output_to_input(from_method_calls, to)
-  matches.map do |match|
-    {
-      output: match[:output],
-      from: match[:name],
-      to: to[:name]
-    }
-  end
-end
-
-def internal_dependencies(from_method_calls:, to:)
-  []
-  #matches = match_
-end
+#def output_dependencies(from_method_calls:, to:)
+#  #puts '---', from_method_calls.inspect, to.inspect
+#  matches = match_output_to_input(from_method_calls, to)
+#  matches.map do |match|
+#    {
+#      output: match[:output],
+#      from: match[:name],
+#      to: to[:name]
+#    }
+#  end
+#end
 
 def new_method_call(from:)
   parameters = from.binding.eval "method(__method__).parameters.map { |p| eval p.last.to_s }"
-  #puts '---', from.method_id, parameters.inspect
   {
     name: from.method_id,
-    input: parameters,
+    input: parameters
+  }
+end
+
+def new_method_return(from:)
+  {
+    name: from.method_id,
     output: from.return_value
   }
 end
 
+def trace_method_calls_to(method_return)
+  []
+end
+
 TracePoint.trace do |trace|
-  @identity ||= []
+  @calls ||= []
+  @returns ||= []
   @dependencies ||= []
   case trace.event
   when :call
     parameters = trace.binding.eval "method(__method__).parameters.map { |p| eval p.last.to_s }"
-    # Hook call with return by tracing the trace.event.object_id
-    puts '+++', trace.method_id, trace.event.object_id
+    @calls.push new_method_call(from: trace)
   when :return
-    puts '???', trace.method_id, trace.event.object_id
-    method_call = new_method_call from: trace
-    @dependencies.concat output_dependencies from_method_calls: @identity, to: method_call
-    @dependencies.concat internal_dependencies from_method_calls: @identity, to: method_call
-    @identity.push method_call
+    method_return = new_method_return from: trace
+    @dependencies.concat trace_method_calls_to(method_return)
+    #@dependencies.concat output_dependencies from_method_calls: @identity, to: method_call
+    #@dependencies.concat internal_dependencies from_method_calls: @identity, to: method_call
+    @returns.push method_return
   when :b_return, :c_return
   else
   end
-  storage.store 'identity', @identity
+  storage.store 'calls', @calls
+  storage.store 'returns', @returns
   storage.store 'dependencies', @dependencies
-  storage.store 'canon', @identity unless established_canon
 end
 
 # don't put anything here unless you want it traced
